f456abf43d68f63cf4ed2f1a4824c78ae7c5534d
ihor-0406 <sevcenkoi461@gmail.com>
2024-09-15 13:06:32 +0200

    lesson01

---
+++ 01/scripts/main.js
@@ -0,0 +1,36 @@
+//змінна  userNickname зі значенням типу стрінг
+
+let userNickname ='user123';
+
+//змінна  userScore  з числовим значення
+
+let userScore = 120924;
+
+// зміннa age + додаємо їй 5 років і виводемо оновлнні данні у консоль
+
+let age = 27;
+console.log(age);
+
+age = age + 5;
+console.log(age);
+
+//зміннa isStudent із значення true
+
+let isStudent = true;
+console.log(isStudent);
+
+//змінюємо наш isStudent на false
+
+isStudent = false;
+console.log(isStudent);
+
+//змінна item яке ми змінюемо декілька разів і виводемо все це у консоль.
+
+let item = 10;
+console.log(item);
+
+item = "ten";
+console.log(item);
+
+item = true;
+console.log(item);
